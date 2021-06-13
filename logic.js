09c2a3f4434b2c1942cb97cb2e39c3273f24eb37
generalustelaru <57430865+generalustelaru@users.noreply.github.com>
2021-06-13 14:31:47 +0300

    Update logic.js
    Reshuffled functions.

--- logic.js
+++ logic.js
@@ -8,6 +8,18 @@ var correct, buttons;
     ()
 )
 
+function populate() { // Create buttons
+        for (let i = 1; i <= buttons; i++) {
+            let newButton = document.createElement("button");
+            newButton.type = "button";
+            newButton.className = "btn btn-secondary";
+            newButton.id = i;
+            newButton.onclick = function(){verify(i)};
+            newButton.innerHTML = i;
+            document.getElementById('pool').appendChild(newButton);
+        }
+}
+
 function randomize() { // Generate a random number between 1 and the number of buttons and store it in 'correct'
     correct = Math.round(Math.random() * buttons);
     if (correct == 0) {
@@ -27,18 +39,6 @@ function verify(n) { // Check selected button against 'correct'
     }                    
     else
         document.getElementById(n).disabled = true;
-}
-
-function populate() { // Create buttons
-        for (let i = 1; i <= buttons; i++) {
-            let newButton = document.createElement("button");
-            newButton.type = "button";
-            newButton.className = "btn btn-secondary";
-            newButton.id = i;
-            newButton.onclick = function(){verify(i)};
-            newButton.innerHTML = i;
-            document.getElementById('pool').appendChild(newButton);
-        }
 }
 
 function reset() { // Remove all buttons, reset tip text, then populate() and randomize()
